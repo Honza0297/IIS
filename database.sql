88c031cdeeabfd3cd4e791951ba9c504fe7942f2
denny101603 <43618398+denny101603@users.noreply.github.com>
2019-10-21 22:22:00 +0200

    upraveno pořadí tvorby tabulek + uprava attachments

--- database.sql
+++ database.sql
@@ -1,17 +1,21 @@
-create table Tickets(ticketID int not null auto_increment, title varchar(128) not null, info text, state ENUM("pending", "in progress", "solved", "canceled", "refused", "retired") not null, date_posted date not null, author int not null, product int not null, 
-    primary key (ticketID), 
-    foreign key (author) references Persons(personID),
-    foreign key (product) references Products(productID));
+CREATE DATABASE `db-its` CHARACTER SET utf8 COLLATE utf8_czech_ci;
 
-create table Attachments(attachmentID int not null auto_increment, ticketID int not null, content blob not null, 
-    primary key (ticketID, attachmentID), 
-    foreign key (ticketID) references Tickets(ticketID));
-
+create table Persons(personID int not null auto_increment, name varchar(64) not null, surname varchar(128) not null, role ENUM("customer", "worker", "manager", "senior manager", "admin"), password varchar(128),
+    primary key (personID));
 
 create table Products(productID int not null auto_increment, product_name text not null, parent_product int, manager int not null, 
     primary key (productID),
     foreign key (parent_product) references Products(productID), 
     foreign key (manager) references Persons(personID));
+
+create table Tickets(ticketID int not null auto_increment, title varchar(128) not null, info text, state ENUM("pending", "in progress", "solved", "canceled", "refused", "retired") not null, date_posted date not null, author int not null, product int not null, 
+    primary key ('ticketID'), 
+    foreign key ('author') references 'Persons'('personID'),
+    foreign key ('product') references 'Products'('productID'));
+
+create table Attachments(ID int not null auto_increment, ticketID int not null, content blob not null, 
+    primary key (ID), 
+    foreign key (ticketID) references Tickets(ticketID));
 
 create table Tasks(taskID int not null auto_increment, task_type ENUM("Bugfix", "Todo", "Feature") not null, state ENUM("pending", "in progress", "solved", "canceled", "refused") not null, ticketID int not null, 
     primary key (taskID),
@@ -21,9 +25,6 @@ create table Work_on_tasks(taskID int not null, personID int not null,
     primary key (personID, taskID),
     foreign key (personID) references Persons(personID),
     foreign key (taskID) references Tasks(taskID));
-
-create table Persons(personID int not null auto_increment, name varchar(64) not null, surname varchar(128) not null, role ENUM("customer", "worker", "manager", "senior manager", "admin"), password varchar(128),
-    primary key (personID));
     
 create table Comments(commentID int not null auto_increment, ticketID int not null, comment_text text not null, date_posted date not null, author int not null,
     primary key (commentID, ticketID),
@@ -55,17 +56,11 @@ insert into Work_on_tasks(taskID, personID) values(1,3);
 
 /*
 select * from Persons;
-
 select * from Products;
-
 select * from Tickets;
-
 select * from Attachments;
-
 select * from Comments;
-
 select * from Work_on_tasks;
-
 select * from Tasks;
 */
 
