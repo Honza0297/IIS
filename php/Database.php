ad7a427306709fd956553401643c804890821160
Honza0297 <33093955+Honza0297@users.noreply.github.com>
2019-11-03 19:14:44 +0100

    Add connection string for eva db
    Add a way how to connect to database on eva server. however, it has to be connection string not variables (it did not work), so values are only informational.

--- php/Database.php
+++ php/Database.php
@@ -8,6 +8,16 @@
 
 class Database
 {
+     /*
+     For db on Eva server, these values are used, however hardcoded:
+     
+     private $socket = '/var/run/mysql/mysql.sock';
+     private $host = 'localhost';
+     private $dbName = 'xberan43';
+     private $username = 'xberan43';
+     private $password = 'i7ezingi';
+      */
+
     public $connection;
 
     private $host = "localhost";
@@ -20,6 +30,9 @@ class Database
         $this->connection = null;
         try
         {
+          /*For connection to Eva, use this:
+           $this->connection = new PDO("mysql:host=localhost;dbname=xberan43;unix_socket=/var/run/mysql/mysql.sock", "xberan43" , "i7ezingi");
+          */
             $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbName, $this->username, $this->password);
             $this->connection->exec("set names utf8");
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
