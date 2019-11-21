4251c68f0f69ea0e8497d4b0334c2433383e9f90
karitale <37588638+karitale@users.noreply.github.com>
2019-11-21 19:06:09 +0100

    prihlasovani
    prihlasovani

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+// Initialize the session
+
+
+	session_start();
+
+	unset($_SESSION["loggedin"]);
+	unset($_SESSION["id"]);
+	unset($_SESSION["role"]);
+	header("Location: ".$_GET["page"]);
+	exit;
+?>
