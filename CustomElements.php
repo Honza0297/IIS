6b47632bd0219db888f98e264444c95ea8819fc1
Daniel Bubeníček <xbuben05@vutbr.cz>
2019-11-25 21:16:35 +0100

    updated function Showselectstatus

--- CustomElements.php
+++ CustomElements.php
@@ -8,16 +8,24 @@
 
 $states = ["", "in progress", "solved", "pending", "canceled", "refused", "retired"];
 
-function ShowSelectStatus($values, $default, $label, $name)
+/**
+ * @param $values array of values (IDs etc) of showed items (LENGTH MUST BE THE SAME AS SHOWEDLABELS)
+ * @param $showedLabels array of strings of showed items
+ * @param $default string - value of item which is supposed to be default
+ * @param $label string - label for the select field
+ * @param $name string - name of the select element
+ */
+function ShowSelectStatus($values, $showedLabels, $default, $label, $name)
 {
     echo "<li><label for=\"$name\">$label:</label><select name=\"$name\" size=\"1\">";
-    foreach ($values as $value)
+    for ($i = 0; $i < count($values); $i++)
     {
+        $value = $values[$i];
+        $showedLabel = $showedLabels[$i];
         if($value == $default)
-            echo "<option value=\"$value\" selected>$value";
+            echo "<option value=\"$value\" selected>$showedLabel";
         else
-            echo "<option value=\"$value\">$value";
-
+            echo "<option value=\"$value\">$showedLabel";
     }
     echo "</select></li>";
 }
