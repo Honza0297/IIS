d080f97658cd68032cb948f8f9c200e1213f972e
Jan Beran <xberan43@vutbr.cz>
2019-11-26 11:08:58 +0100

    Product version 2
    Now it should be functional

--- product.php
+++ product.php
@@ -26,6 +26,7 @@
 
     include_once "php/Database.php";
     include_once "php/model/Product.php";
+    include_once "php/model/Person.php";
 
     $db = new Database();
     $db->getConnection();
@@ -38,10 +39,15 @@
         }
         $product->name = $_POST["productname"];
         $product->description = $_POST["description"];
-        $product->manager = $_POST["manager"];
-        $product->parent_product = $_POST["parent_product"];
+        $product->manager = new \model\Person($db->connection);
+        $product->manager->id = $_POST["manager"];
+        if(isset($_POST["parent_product"]))
+        {
+            $product->parent_product = new \model\Product($db->connection);
+            $product->parent_product->id = $_POST["parent_product"];
+        }
 
-        if($product->name == null or $product->manager)
+        if($product->name == null or $product->manager == null)
         {
             echo "Vratte se tlacitkem zpet a vyplnte vsechny udaje prosim. \n";
             exit();
@@ -50,9 +56,9 @@
         {
             echo $product->id;
             echo $product->name;
-            echo $product->parent_product;
+            echo $product->parent_product == null ? "" : $product->parent_product->id;
             echo $product->description;
-            echo $product->manager;
+            echo $product->manager->name;
 
             if ($product->save()) //novy produkt ulozen
                 echo "Zmeny byly ulozeny\n";
@@ -64,7 +70,7 @@
             $same_name = \model\Product::getByName($product->name, $db->connection);
             if ($same_name == null)
             {
-                $person->save(); //nova osoba ulozena
+                $product->save(); //nova osoba ulozena
                 echo "Produkt zapsan uspesne. :)\n";
                 exit();
             }
@@ -89,11 +95,21 @@
 
         else if ($_GET["action"]=="edit"){
             $product = \model\Product::getByID($_GET["productid"], $db->connection);
+            if(!$product->loadModels())
+            {
+                echo "nepodarilo se ancist modely\n";
+            }
             echo "<form method=\"post\" action=\"product.php\">";
-            echo "<label for=\"productname\">Product name:</label><input id=\"productname\" name=\"productname\" type=\"text\"><br>";
+            echo "<label for=\"productname\">Product name:</label><input id=\"productname\" name=\"productname\" value=\"$product->name\" type=\"text\"><br>";
             echo "<label>Description:</label><br>";
-            echo "<textarea id=\"description\" name=\"description\" rows=\"10\" cols=\"50\"></textarea><br>";             echo "<label for=\"parent\">Parent product:</label><input id=\"parent\"  name=\"parent\" type=\"text\"><br>";
-            echo "<label for=\"manager\">Manager:</label><input id=\"manager\"  name=\"manager\" type=\"text\"><br>";
+            echo "<textarea id=\"description\" name=\"description\"  rows=\"10\" cols=\"50\">$product->description</textarea><br>";
+            $parent_product_name = $product->parent_product == null ? "" : $product->parent_product->name;
+            echo "<label for=\"parent\">Parent product:</label><input id=\"parent\"  name=\"parent\" value=\"$parent_product_name\" type=\"text\"><br>";
+            $username = $product->manager->username;
+            echo "<label for=\"managername\">Manager:</label><input id=\"managername\"  name=\"managername\" value=\"$username\" type=\"text\"><br>";
+
+            $manager_id = $product->manager->id;
+            echo "<label for=\"manager\"></label><input id=\"manager\"  name=\"manager\" value=\"$manager_id\" hidden=\"true\" type=\"text\"><br>";
             $id = $_GET["productid"];
             echo "<input id=\"id\"  name=\"id\" type=\"id\"  hidden=\"true\" value=$id /><br>";
             echo "<input type=\"submit\" value=\"Save changes\" name=\"submit\">";
@@ -108,11 +124,15 @@
             echo "Nepodarilo se stahnout data. Prosim kontaktuje spravce.\n";
             exit();
         }
-
+        if(!$current_product->loadModels())
+        {
+            echo "Nepodarilo se nacist cizi modely\n";
+        }
         echo "<label>Product name: $current_product->name</label><br>";
         echo "<label>Description: $current_product->description</label><br>";
         echo "<label>Parent product: $current_product->parent_product</label><br>";
-        echo "<label>Manager: $current_product->manager</label><br>";
+        $username = $current_product->manager->username;
+        echo "<label>Manager: $username</label><br>";
         {
 
             echo "<a href='product.php?action=edit&productid=";
